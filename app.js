36d29f1dc62c15b9377805bd4a31dea173374d07
SanyaOdare <sanyaodare@gmail.com>
2022-06-03 17:44:55 +0100

    add footer

--- app.js
+++ app.js
@@ -4,3 +4,29 @@ const sportsButton = document.getElementById('sports');
 const techButton = document.getElementById('technology');
 const entertainmentButton = document.getElementById('entertainment');
 const searchButton = document.getElementById('search-button');
+
+// Fetch News Data
+
+generalButton.addEventListener("click", function() {
+
+});
+
+businessButton.addEventListener("click", function() {
+
+});
+
+sportsButton.addEventListener("click", function() {
+
+});
+
+techButton.addEventListener("click", function() {
+
+});
+
+entertainmentButton.addEventListener("click", function() {
+
+});
+
+searchButton.addEventListener("click", function() {
+
+});
