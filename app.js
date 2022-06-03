94436b3da1fa2af3d3e0ab5c62d333abee8480c6
SanyaOdare <sanyaodare@gmail.com>
2022-06-03 17:24:06 +0100

    style the navbar

---
+++ app.js
@@ -0,0 +1,6 @@
+const generalButton = document.getElementById('general');
+const businessButton = document.getElementById('business');
+const sportsButton = document.getElementById('sports');
+const techButton = document.getElementById('technology');
+const entertainmentButton = document.getElementById('entertainment');
+const searchButton = document.getElementById('search-button');
